b131c44df5eddce23a6b0d4575876d192f9b9ae9
jlilly <johannlilly@users.noreply.github.com>
2018-03-28 19:23:38 +0200

    Implement modifier in functions

--- contracts/flipper.sol
+++ contracts/flipper.sol
@@ -4,33 +4,28 @@ contract Flipper {
 	// declare variable of type GameState
 	GameState public currentState;
 
-	modifier onlyState(GameState expectedState) {
-		if(expectedState == currentState) {
-			_; // delegate to the function that uses this modifier.
-		} else {
-			throw; 
-		}
-	}
+	modifier onlyState(GameState expectedState) {if(expectedState == currentState) {_;} else {throw;}}
 
 	function Flipper() {
 		currentState = GameState.noWager;
 	}
 
-	function transitionGameState(bytes32 targetState) returns (bool) {
-		if(targetState == "noWager") {
-			currentState = GameState.noWager;
-			return true;
-		}
-		else if (targetState == "wagerMade") {
-			currentState = GameState.wagerMade;
-			return true;
-		}
-		else if (targetState == "wagerAccepted") {
-			currentState = GameState.wagerAccepted;
-			return true;
-		}
-
-		return false;
+	function makeWager() onlyState(GameState.noWager) returns (bool) {
+		// ,,,
+		currentState = GameState.wagerMade;
+		return true;
 	}
 
+	function acceptWager() onlyState(GameState.wagerMade) returns (bool) {
+		// ...
+		currentState = GameState.wagerAccepted;
+		return true;
+	}
+
+	function resolveBet() onlyState(GameState.wagerAccepted) returns (bool) {
+		// ...
+		currentState = GameState.noWager;
+		return true;
+	}
+	
 }
